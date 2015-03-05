bf42ad32abdd4ed5563514ebdaca8dff393b051a
cesarubidia <cesar.ubidia,hoyos@mail.com>
2015-03-05 10:50:45 -0500

    commit

--- app/src/main/java/com/scpdemo/DisiAndroid/Main/Activity_Main.java
+++ app/src/main/java/com/scpdemo/DisiAndroid/Main/Activity_Main.java
@@ -2,11 +2,21 @@ package com.scpdemo.DisiAndroid.Main;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.DisplayMetrics;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.scpdemo.DisiAndroid.Activity_Busqueda;
+import com.scpdemo.DisiAndroid.Adapter.ListViewAdapter;
+import com.scpdemo.DisiAndroid.Mesa.Activity_Mesa;
 import com.scpdemo.DisiAndroid.Mesa.Activity_Mesa_Lis;
 import com.scpdemo.DisiAndroid.Pedido.Activity_Pedido;
 import com.scpdemo.DisiAndroid.Producto.Activity_Producto;
@@ -21,6 +31,29 @@ import com.scpdemo.DisiAndroid.Usuario.Activity_Usuario_List;
 
 public class Activity_Main extends ActionBarActivity {
     Button btnUsuario,btnCliente,btnMesa,btnProducto,btnPedido,btnFacturar,btnConsulta,btnReporte;
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mLvMenu;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private LinearLayout llMenu;
+
+    ListViewAdapter adapter;
+
+    String[] titulo = new String[]{
+            "Usuarios",
+            "Clientes",
+            "Mesas",
+            "Productos",
+            "Ubicación de Franquicias"
+    };
+
+
+    int[] imagenes = {
+            R.drawable.seguridad3,
+            R.drawable.cliente1,
+            R.drawable.mesa,
+            R.drawable.nota1,
+            R.drawable.ic_gps};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +84,94 @@ public class Activity_Main extends ActionBarActivity {
         btnFacturar.setOnClickListener(btnFacturarOnClickListener);
         btnConsulta.setOnClickListener(btnConsultaOnClickListener);
         btnReporte.setOnClickListener(btnReporteOnClickListener);
+
+
+        llMenu = (LinearLayout) findViewById(R.id.llMenu);
+        mLvMenu = (ListView) findViewById(R.id.listView1);
+
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        DrawerLayout.LayoutParams dllp = (DrawerLayout.LayoutParams) llMenu.getLayoutParams();
+        dllp.width = displayMetrics.widthPixels - getResources().getDimensionPixelOffset(R.dimen.lvmenu_marginright);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(Activity_Main.this, mDrawerLayout, R.string.app_name, R.string.app_name) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+
+        };
+
+
+
+
+        final ListView lista = (ListView) findViewById(R.id.listView1);
+        adapter = new ListViewAdapter(this, titulo, imagenes);
+        lista.setAdapter(adapter);
+
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "presiono " + i, Toast.LENGTH_SHORT).show();
+
+                switch(i) {
+                    case 0:
+                        Intent intent = new Intent(Activity_Main.this,Activity_Mesa.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        Intent intent2 = new Intent(Activity_Main.this,Activity_Mesa.class);
+                        startActivity(intent2);
+                        break;
+                    case 2:
+                        Intent intent3 = new Intent(Activity_Main.this,Activity_Mesa.class);
+                        startActivity(intent3);
+                        break;
+                    case 3:
+                        Intent intent4 = new Intent(Activity_Main.this,Activity_Mesa.class);
+                        startActivity(intent4);
+                        break;
+                    case 4:
+                        Intent intent5 = new Intent(Activity_Main.this,Activity_Mesa.class);
+                        startActivity(intent5);
+                        break;
+
+                }
+
+            }
+        });
+
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "presiono LARGO " + i, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mActionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+        else
+            return super.onOptionsItemSelected(item);
+    }
+
+
 
     View.OnClickListener btnUsuarioOnClickListener= new View.OnClickListener() {
         @Override
