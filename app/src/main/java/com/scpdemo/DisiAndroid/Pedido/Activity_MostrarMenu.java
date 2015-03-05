6c566d6ba67d07ff6a1e26bc4424b4f9b60b7119
andresfukazawa <fukazawa@gmail.com>
2015-03-04 23:54:58 -0500

    Implementacion de pagina de pedidos

---
+++ app/src/main/java/com/scpdemo/DisiAndroid/Pedido/Activity_MostrarMenu.java
@@ -0,0 +1,89 @@
+package com.scpdemo.DisiAndroid.Pedido;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.scpdemo.DisiAndroid.Adapter.LVMainAdapter;
+import com.scpdemo.DisiAndroid.DAO.DataBaseHelper;
+import com.scpdemo.DisiAndroid.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Susumu on 3/4/2015.
+ */
+public class Activity_MostrarMenu extends ActionBarActivity {
+
+    private TextView tvMesa;
+    private EditText etFiltro;
+    private ListView lvMenu;
+    private LVMainAdapter     mLVMainAdapter;
+    private ArrayList<String> mLstString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrarmenu);
+
+        tvMesa   = (TextView)findViewById(R.id.tv_mm_SMMesa);
+        etFiltro = (EditText)findViewById(R.id.et_mm_SMFiltro);
+        lvMenu   = (ListView)findViewById(R.id.lv_mm_SMMenu);
+        Toast.makeText(Activity_MostrarMenu.this, "in onCreate", Toast.LENGTH_SHORT).show();
+        populateList(getIntent().getExtras().getString("tipo"));
+        //populateList("1");
+        mLVMainAdapter = new LVMainAdapter(Activity_MostrarMenu.this, 0, mLstString);
+
+        lvMenu.setAdapter(mLVMainAdapter);
+        etFiltro.addTextChangedListener(etMenuTextWatcher);
+    }
+
+    private void populateList(String tipo) {
+        Cursor cursor = null;
+        mLstString = new ArrayList<>();
+
+        Toast.makeText(Activity_MostrarMenu.this, "in populateList", Toast.LENGTH_SHORT).show();
+
+        try{
+            cursor = DataBaseHelper.myDataBase.query("PRODUCTO", null, "PRODTIP=?", new String[]{tipo}, null, null, "PRODNOM");
+            if(cursor.moveToFirst()) {
+                do {
+                    mLstString.add(cursor.isNull(cursor.getColumnIndex("PRODNOM")) ? "" : cursor.getString(cursor.getColumnIndex("PRODNOM")));
+                } while (cursor.moveToNext());
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+
+    }
+
+    TextWatcher etMenuTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            Log.d("watcher", s.toString());
+            mLVMainAdapter.getFilter().filter(s.toString());
+            mLVMainAdapter.notifyDataSetChanged();
+        }
+    };
+}
--- app/src/main/java/com/scpdemo/DisiAndroid/Pedido/Activity_Pedido.java
+++ app/src/main/java/com/scpdemo/DisiAndroid/Pedido/Activity_Pedido.java
@@ -1,27 +1,124 @@
 package com.scpdemo.DisiAndroid.Pedido;
 
-import android.app.Activity;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.scpdemo.DisiAndroid.Producto.Fragment_Cliente;
-import com.scpdemo.DisiAndroid.Producto.Fragment_Pedido;
-import com.scpdemo.DisiAndroid.Producto.Fragment_Producto;
+import com.scpdemo.DisiAndroid.DAO.DataBaseHelper;
 import com.scpdemo.DisiAndroid.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Susumu on 3/3/2015.
  */
 public class Activity_Pedido extends ActionBarActivity {
 
+    private Spinner spMesa;
+    private Button  btEntradas, btPFondo, btPostres;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedido);
+
+        spMesa     = (Spinner)findViewById(R.id.sp_pe_mesa);
+        btEntradas = (Button)findViewById(R.id.bt_pe_entradas);
+        btPFondo   = (Button)findViewById(R.id.bt_pe_pfondo);
+        btPostres  = (Button)findViewById(R.id.bt_pe_postres);
+
+        populateSpinner();
+
+        btEntradas.setOnClickListener(btEntradasOnClick);
+        btPFondo.setOnClickListener(btPFondoOnClick);
+        btPostres.setOnClickListener(btPostresOnClick);
+
+
+        spMesa.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View arg1, int arg2, long arg3) {
+                ((TextView) parent.getChildAt(0)).setTextSize(48);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
     }
+
+    private void populateSpinner() {
+
+        List<String> list = new ArrayList<String>();
+        Cursor cursor = null;
+
+        try{
+            cursor = DataBaseHelper.myDataBase.query("MESA", new String[]{"MESACOD"}, null, null, null, null, "MESACOD");
+            if(cursor.moveToFirst()) {
+                do {
+                    list.add(cursor.getInt(cursor.getColumnIndex("MESACOD"))+"");
+                } while (cursor.moveToNext());
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(R.layout.spinner_item);
+        spMesa.setAdapter(dataAdapter);
+    }
+
+    private void mostrarMenu(String tipo, String mesa) {
+        Toast.makeText(Activity_Pedido.this, "in mostrarMenu", Toast.LENGTH_SHORT).show();
+        Toast.makeText(Activity_Pedido.this, "tipo : " + tipo, Toast.LENGTH_SHORT).show();
+        Toast.makeText(Activity_Pedido.this, "mesa : " + mesa, Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(Activity_Pedido.this, Activity_MostrarMenu.class);
+        intent.putExtra("tipo", tipo);
+        intent.putExtra("mesa", mesa);
+        startActivity(intent);
+    }
+
+    View.OnClickListener btEntradasOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(Activity_Pedido.this, spMesa.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            mostrarMenu("1", spMesa.getSelectedItem().toString());
+        }
+    };
+
+//    View.OnClickListener btEntradasOnClick = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            Toast.makeText(Activity_Pedido.this, "in View.OnClickListener", Toast.LENGTH_SHORT).show();
+//            mostrarMenu("1", spMesa.getSelectedItem().toString());
+//        }
+//    };
+
+    View.OnClickListener btPFondoOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(Activity_Pedido.this, "in View.OnClickListener", Toast.LENGTH_SHORT).show();
+            mostrarMenu("2", spMesa.getSelectedItem().toString());
+        }
+    };
+
+    View.OnClickListener btPostresOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(Activity_Pedido.this, "in View.OnClickListener", Toast.LENGTH_SHORT).show();
+            mostrarMenu("3", spMesa.getSelectedItem().toString());
+        }
+    };
 }
