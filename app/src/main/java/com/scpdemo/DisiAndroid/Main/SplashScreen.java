74c5cef6fb9697dec021ad6ecd174c1dce48bd61
cesarubidia <cesar.ubidia,hoyos@mail.com>
2015-03-13 17:07:06 -0500

    commit

--- app/src/main/java/com/scpdemo/DisiAndroid/Main/SplashScreen.java
+++ app/src/main/java/com/scpdemo/DisiAndroid/Main/SplashScreen.java
@@ -1,10 +1,15 @@
 package com.scpdemo.DisiAndroid.Main;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.TextView;
 
 import com.scpdemo.DisiAndroid.R;
 
@@ -15,20 +20,15 @@ import java.util.TimerTask;
  * Created by Renan on 12/02/2015.
  */
 public class SplashScreen extends Activity {
-    // Set the duration of the splash screen
-    private static final long SPLASH_SCREEN_DELAY = 3000;
 
+    private static final long SPLASH_SCREEN_DELAY = 5000;
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
-     //   setContentView(R.layout.splashscreem);
-     //   Resources res= getResources();
-     //   Resources drawable = res.getDrawable(R.drawable.shape_presentacion);
-      //  textView tv_shape =(TextView) findViewByid(tv_shape);
-
-      //  tv_shape.setBackgroundDrawable(drawable);
 
 
         // Set portrait orientation
@@ -56,5 +56,11 @@ public class SplashScreen extends Activity {
         // Simulate a long loading process on application startup.
         Timer timer = new Timer();
         timer.schedule(task, SPLASH_SCREEN_DELAY);
+
+        setContentView(R.layout.splashscreem);
+        Resources res = getResources();
+        Drawable drawable = res.getDrawable(R.drawable.shape_presentacion);
+        TextView tv_shape =(TextView)findViewById(R.id.tv_principal);
+        tv_shape.setBackground(drawable);
     }
 }
